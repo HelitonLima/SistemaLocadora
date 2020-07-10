092cb14b37a0b6e315dfdcb4b92eebb596c5165b
Heliton Martins de Lima <43296998+HelitonLima@users.noreply.github.com>
2020-07-10 18:34:21 -0300

    Add files via upload

---
+++ TrabalhoDS/TrabalhoDS/Conexão/ConnectionFactory.cs
@@ -0,0 +1,20 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace TrabalhoDS.Conexão
+{
+    public class ConnectionFactory
+    {
+        public static MySqlConnection getConnection()
+        {
+            string conexao = ConfigurationManager.ConnectionStrings["locadora_2iiem_1sem_2018"].ConnectionString;
+
+            return new MySqlConnection(conexao);
+        }
+    }
+}
